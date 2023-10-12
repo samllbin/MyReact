c34dfc3afd66df089b18aff5f60148715d9181b2
samllbin <3057988675@qq.com>
2023-10-12 22:08:37 +0800

    feat: 修改多次placement错误

--- packages/react-dom/node_modules/react-reconciler/src/fiberFlags.ts
+++ packages/react-dom/node_modules/react-reconciler/src/fiberFlags.ts
@@ -1,12 +1,12 @@
 export type Flags = number;
 
-export const NoFlags = 0b0000001;
+export const NoFlags = 0b0000000;
 //属性相关
-export const Update = 0b000100;
+export const Update = 0b0000010;
 
 //结构相关
-export const ChildDeletion = 0b001000;
-export const Placement = 0b000010;
+export const ChildDeletion = 0b0000100;
+export const Placement = 0b0000001;
 
 //某个阶段需要执行的操作
 export const MutationMask = Placement | Update | ChildDeletion;
