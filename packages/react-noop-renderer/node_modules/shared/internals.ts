224cb7623e02365b71ac6efaae24a33ae72c0ae2
samllbin <3057988675@qq.com>
2023-10-22 16:40:00 +0800

    feat: 实现noop-renderer包

---
+++ packages/react-noop-renderer/node_modules/shared/internals.ts
@@ -0,0 +1,6 @@
+//为内部数据共享层中转
+import * as React from 'react';
+
+const internals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+
+export default internals;
--- scripts/rollup/utils.js
+++ scripts/rollup/utils.js
@@ -1,33 +1,33 @@
 import path from 'path';
 import fs from 'fs';
+
 import ts from 'rollup-plugin-typescript2';
 import cjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 
 const pkgPath = path.resolve(__dirname, '../../packages');
 const distPath = path.resolve(__dirname, '../../dist/node_modules');
-//实现解析package.json中的name函数
+
 export function resolvePkgPath(pkgName, isDist) {
 	if (isDist) {
 		return `${distPath}/${pkgName}`;
-	} else {
-		return `${pkgPath}/${pkgName}`;
 	}
+	return `${pkgPath}/${pkgName}`;
 }
 
 export function getPackageJson(pkgName) {
-	//...包路径
+	// ...包路径
 	const path = `${resolvePkgPath(pkgName)}/package.json`;
 	const str = fs.readFileSync(path, { encoding: 'utf-8' });
 	return JSON.parse(str);
 }
 
-export function getBaseRollupPlugins(
+export function getBaseRollupPlugins({
 	alias = {
 		__DEV__: true,
 		preventAssignment: true
 	},
-	{ typescript = {} } = {}
-) {
+	typescript = {}
+} = {}) {
 	return [replace(alias), cjs(), ts(typescript)];
 }
